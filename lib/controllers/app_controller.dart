a4be37ce150cd46e688dade9ca6faaf6e2bd8c87
Jonghoon Kim <uaremine@gmail.com>
2021-08-09 15:01:29 +0900

    heads up notification 구현

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -1,6 +1,7 @@
 // app_controller.dart
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 
 class AppController extends GetxController {
@@ -23,8 +24,53 @@ class AppController extends GetxController {
       sound: true,
     );
 
+    // iOS foreground에서 heads up display 표시를 위해 alert, sound true로 설정
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, // Required to display a heads up notification
+      badge: true,
+      sound: true,
+    );
+
+    // Android용 새 Notification Channel
+    const AndroidNotificationChannel androidNotificationChannel = AndroidNotificationChannel(
+      'high_importance_channel', // 임의의 id
+      'High Importance Notifications', // 설정에 보일 채널명
+      'This channel is used for important notifications.', // 설정에 보일 채널 설명
+      importance: Importance.max,
+    );
+
+    // Notification Channel을 디바이스에 생성
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(androidNotificationChannel);
+
+    // FlutterLocalNotificationsPlugin 초기화. 이 부분은 notification icon 부분에서 다시 다룬다.
+    await flutterLocalNotificationsPlugin.initialize(
+        InitializationSettings(
+            android: AndroidInitializationSettings('@mipmap/ic_launcher'), iOS: IOSInitializationSettings()),
+        onSelectNotification: (String? payload) async {});
+
     FirebaseMessaging.onMessage.listen((RemoteMessage rm) {
       message.value = rm;
+      RemoteNotification? notification = rm.notification;
+      AndroidNotification? android = rm.notification?.android;
+
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          0,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              'high_importance_channel', // 임의의 id
+              'High Importance Notifications', // 설정에 보일 채널명
+              'This channel is used for important notifications.', // 설정에 보일 채널 설명
+              // other properties...
+            ),
+          ),
+        );
+      }
     });
 
     return true;
